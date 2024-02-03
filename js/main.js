e447f03334120968ae8d2d22c3093a9b583fc0f4
marcosc1995 <marcos_c95@outlook.com>
2024-02-03 17:15:47 -0300

    Update
    agregado la seccion ToDo y solucionado los problemas mayores

--- js/main.js
+++ js/main.js
@@ -2,29 +2,30 @@ const mask = document.getElementById("mask");
 const main = document.getElementById("mainBox");
 const site = document.getElementById("siteUnderConstruction");
 const hi = document.getElementById("hi");
-const nameTitle = document.getElementById('nameTitle');
-const title = document.getElementById('title');
+const nameTitle = document.getElementById("nameTitle");
+const title = document.getElementById("title");
 // ToDo sections
-const section1 = document.getElementById('toDoSection1')
-const section2 = document.getElementById('toDoSection2')
+const section1 = document.getElementById("toDoSection1");
+const section2 = document.getElementById("toDoSection2");
 
+window.addEventListener("scroll", function () {
+  let section1 = document.querySelector("#toDoSection1");
+  let section1Position = section1.getBoundingClientRect().top;
+  let section2 = document.querySelector("#toDoSection2");
+  let section2Position = section1.getBoundingClientRect().top;
+  let screenPosition = window.innerHeight / 1.3;
 
-
-
-window.addEventListener('scroll', function () {
-    let content = document.querySelector('#siteUnderConstruction')
-    let contentPosition = content.getBoundingClientRect().top;
-    let screenPosition = window.innerHeight / 1.3;
-
-    if(contentPosition < screenPosition){
-        content.classList.remove('hidden')
-        content.classList.add('visible')
-
-    }else{
-        content.classList.remove('visible')
-        content.classList.add('hidden')
-    }
-})
+  if (section1Position < screenPosition) {
+    console.log("test");
+    section1.classList.remove("hidden");
+    section1.classList.add("visible");
+  }
+  if(section2Position < screenPosition){
+    console.log('test 2')
+    section2.classList.remove("hidden");
+    section2.classList.add("visible");
+  }
+});
 
 function loadIntro() {
   setTimeout(() => {
@@ -33,19 +34,18 @@ function loadIntro() {
   setTimeout(() => {
     main.classList.remove("hidden");
     main.classList.add("visible");
-    
   }, 2000);
   setTimeout(() => {
-    hi.classList.remove('hidden');
-    hi.classList.add('visible');
+    hi.classList.remove("hidden");
+    hi.classList.add("visible");
   }, 2200);
   setTimeout(() => {
     nameTitle.classList.remove("hidden");
-    nameTitle.classList.add("visible");    
+    nameTitle.classList.add("visible");
   }, 2500);
   setTimeout(() => {
     title.classList.remove("hidden");
-    title.classList.add("visible");       
+    title.classList.add("visible");
   }, 2800);
 }
 loadIntro();
